8ca7d0e1b0572bd5494e8c2234f9c5412333e7ee
Marek <marekstankiewicz6@wp.pl>
2024-12-06 13:26:04 +0100

    added rxjs operators finished course

--- first-angular-app/src/app/search/search.component.ts
+++ first-angular-app/src/app/search/search.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { debounceTime, elementAt, filter, first, from, last, Observable, of, take, takeLast, takeWhile } from 'rxjs';
+import { count, debounceTime, distinct, elementAt, filter, first, from, last, max, min, Observable, of, skip, take, takeLast, takeWhile } from 'rxjs';
 
 @Component({
   selector: 'app-search',
@@ -14,8 +14,12 @@ export class SearchComponent implements OnInit {
   searchForm: FormGroup;
   // name: FormControl;
 
-  categories = ['MObile', 'TV', 'Chargers', 'Headphones'];
+  categories = ['MObile', 'TV', 'TV', 'Chargers', 'Chargers', 'Headphones'];
   category$: Observable<string> = from(this.categories);
+
+
+  ranks = [1,2,3,4,5,6,33,76];
+  rank$: Observable<number> = from(this.ranks);
 
   contructor(private formBuilder: FormBuilder) {}
 
@@ -31,7 +35,23 @@ export class SearchComponent implements OnInit {
       // takeWhile((v) => this.checkCondition(v)),
       // debounceTime(3000)
     ).subscribe(data => {
-      console.log(data);
+      this.rank$
+      .pipe(
+        distinct(),
+        // filter((v) => this.filterValues(v)),
+        // max()
+        filter((v) => this.filterValues(v)),
+        min()
+      )
+      .subscribe(data => {
+        // console.log(`Maximum value is: ` + data);
+        console.log(`Minimum value is: ` + data);
+      })
+      this.category$.pipe(
+        // distinct(),
+        // skip(2)
+        count()
+      );
       // this.category$.pipe(
         // takeLast(2)
         // first()
@@ -41,6 +61,10 @@ export class SearchComponent implements OnInit {
       //   console.log(data);
       // })
     })
+  }
+
+  filterValues(v) {
+    return v.length < 10 ? true : false;
   }
 
   checkCharCount(v) {
